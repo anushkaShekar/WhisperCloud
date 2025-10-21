3a4484fe4f98b345275a436265d3e8ce92e9dbf4
Anushka Chandrashekar <achandrashe4@wisc.edu>
2025-10-20 17:19:40 -0700

    Login authentication + user landing page

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -9,6 +9,43 @@ router.get("/login",(req,res)=>{
         title: "Login | WhisperCloud"
     });
 })
+
+router.post("/signin", async(req, res) => {
+    try {
+        const {email, password} = req.body
+        console.log(email)
+        const userData= await User.find({email: {$eq: email}})
+        console.log(userData.length)
+
+        // Check if user's email is present in the database
+        if (userData.length != 0) {
+            const userPassword = userData[0].password
+
+            // Check if the passwords match
+            bcrypt.compare(password, userPassword, (err, data) => {
+                if (err) throw err
+
+                if (data) {
+                    res.redirect("/homepage")
+                } else {
+                    return res.status(401).json({msg: "The inputted password did not mach the account holder's password. Please try again."})
+                }
+            })
+        } else {
+            return res.status(401).json({msg: "The inputted email address was not found in our system. Please try again or sign up for an account if you haven't already."})
+        }
+    } catch (error) {
+        console.log(error)
+    }
+
+})
+
+router.get("/homepage", (req, res) => {
+    res.render("./user/homepage", {
+        title: "Homepage | WhisperCloud"
+    })
+})
+
 router.get("/signup",(req,res)=>{
     res.render("./user/signup",{
         title: "Sign Up | WhisperCloud"
